007a39fd7a6933e692155ced35a5c76f89b5617f
txkiz <txkiz@ADMIN>
2023-03-26 13:57:26 +0300

    Добавьте файлы проекта.

---
+++ Zmeika/Zmeika.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define WIDTH 120
+#define HEIGHT 30
+
+void TMAP() {
+	char size[HEIGHT][WIDTH];
+
+	for (int j = 0; j <= HEIGHT; j++)
+	{
+		for (int i = 0; i <= WIDTH; i++)
+		{
+			size[j][i] = ' ';
+		}
+		size[j][WIDTH] = '\0';
+		cout << size[j][WIDTH];
+	}
+}
+
+int main()
+{
+	TMAP();
+
+	return 0;
+}
